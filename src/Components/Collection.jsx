a1be0f60c213beebc7137ef012573753f0b5db5a
Zaheerabbas <zabbas092002@gmail.com>
2024-09-14 17:30:38 +0500

    add the images in  collection section

--- src/Components/Collection.jsx
+++ src/Components/Collection.jsx
@@ -3,19 +3,30 @@ import React from "react";
 import "./Collection.css";
 import Cards from "./Cards"; // Correct import
 import image from "../assets/images/ecommerce.webp";
+import image1 from "../assets/images/image-1.png";
+import image2 from "../assets/images/image-2.png";
+import image3 from "../assets/images/image-3.png";
+import image4 from "../assets/images/image-4.png";
+import image5 from "../assets/images/image-5.png";
+import image6 from "../assets/images/image-6.png";
+import image7 from "../assets/images/image-7.png";
+import image8 from "../assets/images/image-8.png";
+import image9 from "../assets/images/image-9.png";
+import image10 from "../assets/images/image-10.png";
+
 
 const Collection = () => {
   const cardData = [
-    { image: image, title: "Product 1", price: "$76" },
-    { image: image, title: "Product 2", price: "$85" },
-    { image: image, title: "Product 3", price: "$90" },
-    { image: image, title: "Product 4", price: "$120" },
-    { image: image, title: "Product 5", price: "$150" },
-    { image: image, title: "Product 6", price: "$76" },
-    { image: image, title: "Product 7", price: "$85" },
-    { image: image, title: "Product 8", price: "$90" },
-    { image: image, title: "Product 9", price: "$120" },
-    { image: image, title: "Product 10", price: "$150" },
+    { image: image1, title: "Kid Tapered Slim Fit Trouser", price: "$76" },
+    { image: image2, title: "Men Round Neck Pure Cotton T-shirt", price: "$85" },
+    { image: image3, title: "Boy Round Neck Pure Cotton T-shirt", price: "$90" },
+    { image: image4, title: "Women Zip-Front Relaxed Fit Jacket", price: "$120" },
+    { image: image5, title: "Men Tapered Fit Flat-Front Trousers", price: "$150" },
+    { image: image6, title: "PGirls Round Neck Cotton Top", price: "$76" },
+    { image: image7, title: "Women Zip-Front Relaxed Fit Jacket", price: "$85" },
+    { image: image8, title: "Kid Tapered Slim Fit Trouser", price: "$90" },
+    { image: image9, title: "Men Printed Plain Cotton Shirt", price: "$120" },
+    { image: image10, title: "Women Zip-Front Relaxed Fit Jacket", price: "$150" },
   
   
   ];
