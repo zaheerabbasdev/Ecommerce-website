efba345c8adf16916d401955e32dc03853f3f1c0
Zaheerabbas <zabbas092002@gmail.com>
2024-09-15 11:05:46 +0500

    add the subscribe component

--- src/Components/Subscribe.jsx
+++ src/Components/Subscribe.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
-import './Subscribe.css'
+import React from "react";
+import "./Subscribe.css";
 
 const Subscribe = () => {
   return (
     <div>
-        <div className="subscribe-section" id='subscribe'>
-            <div className="row">
-                <div className="col-md-4">
-                    <h4>Subscribe now & get 20% off</h4>
-                    <p>Stay updated with our latest offers and promotions. Subscribe to our newsletter and enjoy an exclusive 20% discount on your next purchase.</p>
-                    <input type="text" name="text" id="text" placeholder='Enter your email'/>
-                    <button>Subscribe</button>
-                </div>
-            </div>
+      <div className="subscribe-section" id="subscribe">
+        <div className="row1">
+          <h4>Subscribe now & get 20% off</h4>
         </div>
+        <div className="row2">
+          <p>
+            Stay updated with our latest offers and promotions. Subscribe to our
+            newsletter and enjoy an exclusive 20% discount on your next
+            purchase.
+          </p>
+        </div>
+        <div className="row3">
+          <input
+            type="text"
+            name="text"
+            id="text"
+            placeholder="Enter your email"
+          />
+          <button>Subscribe</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Subscribe;
