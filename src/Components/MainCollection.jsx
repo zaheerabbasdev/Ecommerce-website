48b91f8a2b20091c66d06045eab650286b822256
Zaheerabbas <zabbas092002@gmail.com>
2024-09-16 14:05:45 +0500

    add maincollection component

--- src/Components/MainCollection.jsx
+++ src/Components/MainCollection.jsx
@@ -11,8 +11,50 @@ import image7 from "../assets/images/image-7.png";
 import image8 from "../assets/images/image-8.png";
 import image9 from "../assets/images/image-9.png";
 import image10 from "../assets/images/image-10.png";
-
+import image11 from "../assets/images/image-11.png";
+import image12 from "../assets/images/image-12.png";
+import image13 from "../assets/images/image-13.png";
+import image14 from "../assets/images/image-14.png";
+import image15 from "../assets/images/image-15.png";
+import image16 from "../assets/images/image-16.png";
+import image17 from "../assets/images/image-17.png";
+import image18 from "../assets/images/image-18.png";
+import image19 from "../assets/images/image-19.png";
+import image20 from "../assets/images/image-20.png";
+import image21 from "../assets/images/image-1.png";
+import image22 from "../assets/images/image-2.png";
+import image23 from "../assets/images/image-3.png";
+import image24 from "../assets/images/image-4.png";
+import image25 from "../assets/images/image-5.png";
+import image26 from "../assets/images/image-6.png";
+import image27 from "../assets/images/image-7.png";
+import image28 from "../assets/images/image-8.png";
+import image29 from "../assets/images/image-9.png";
+import image30 from "../assets/images/image-10.png";
+import image31 from "../assets/images/image-11.png";
+import image32 from "../assets/images/image-12.png";
+import image33 from "../assets/images/image-13.png";
+import image34 from "../assets/images/image-14.png";
+import image35 from "../assets/images/image-15.png";
+import image36 from "../assets/images/image-16.png";
+import image37 from "../assets/images/image-17.png";
+import image38 from "../assets/images/image-18.png";
+import image39 from "../assets/images/image-19.png";
+import image40 from "../assets/images/image-20.png";
+import image41 from "../assets/images/image-11.png";
+import image42 from "../assets/images/image-12.png";
+import image43 from "../assets/images/image-13.png";
+import image44 from "../assets/images/image-14.png";
+import image45 from "../assets/images/image-15.png";
+import image46 from "../assets/images/image-16.png";
+import image47 from "../assets/images/image-17.png";
+import image48 from "../assets/images/image-18.png";
+import image49 from "../assets/images/image-19.png";
+import image50 from "../assets/images/image-20.png";
 const MainCollection = () => {
+
+
+  
   const cardData = [
     { image: image1, title: "Kid Tapered Slim Fit Trouser", price: "$76" },
     {
@@ -48,14 +90,154 @@ const MainCollection = () => {
       title: "Women Zip-Front Relaxed Fit Jacket",
       price: "$150",
     },
+
+    { image: image11, title: "Kid Tapered Slim Fit Trouser", price: "$76" },
+    {
+      image: image12,
+      title: "Men Round Neck Pure Cotton T-shirt",
+      price: "$85",
+    },
+    {
+      image: image13,
+      title: "Boy Round Neck Pure Cotton T-shirt",
+      price: "$90",
+    },
+    {
+      image: image14,
+      title: "Women Zip-Front Relaxed Fit Jacket",
+      price: "$120",
+    },
+    {
+      image: image15,
+      title: "Men Tapered Fit Flat-Front Trousers",
+      price: "$150",
+    },
+    { image: image16, title: "PGirls Round Neck Cotton Top", price: "$76" },
+    {
+      image: image17,
+      title: "Women Zip-Front Relaxed Fit Jacket",
+      price: "$85",
+    },
+    { image: image18, title: "Kid Tapered Slim Fit Trouser", price: "$90" },
+    { image: image19, title: "Men Printed Plain Cotton Shirt", price: "$120" },
+    {
+      image: image20,
+      title: "Women Zip-Front Relaxed Fit Jacket",
+      price: "$150",
+    },
+
+    { image: image21, title: "Kid Tapered Slim Fit Trouser", price: "$76" },
+    {
+      image: image22,
+      title: "Men Round Neck Pure Cotton T-shirt",
+      price: "$85",
+    },
+    {
+      image: image23,
+      title: "Boy Round Neck Pure Cotton T-shirt",
+      price: "$90",
+    },
+    {
+      image: image24,
+      title: "Women Zip-Front Relaxed Fit Jacket",
+      price: "$120",
+    },
+    {
+      image: image25,
+      title: "Men Tapered Fit Flat-Front Trousers",
+      price: "$150",
+    },
+    { image: image26, title: "PGirls Round Neck Cotton Top", price: "$76" },
+    {
+      image: image27,
+      title: "Women Zip-Front Relaxed Fit Jacket",
+      price: "$85",
+    },
+    { image: image28, title: "Kid Tapered Slim Fit Trouser", price: "$90" },
+    { image: image29, title: "Men Printed Plain Cotton Shirt", price: "$120" },
+    {
+      image: image30,
+      title: "Women Zip-Front Relaxed Fit Jacket",
+      price: "$150",
+    },
+
+    { image: image31, title: "Kid Tapered Slim Fit Trouser", price: "$76" },
+    {
+      image: image32,
+      title: "Men Round Neck Pure Cotton T-shirt",
+      price: "$85",
+    },
+    {
+      image: image33,
+      title: "Boy Round Neck Pure Cotton T-shirt",
+      price: "$90",
+    },
+    {
+      image: image34,
+      title: "Women Zip-Front Relaxed Fit Jacket",
+      price: "$120",
+    },
+    {
+      image: image35,
+      title: "Men Tapered Fit Flat-Front Trousers",
+      price: "$150",
+    },
+    { image: image36, title: "PGirls Round Neck Cotton Top", price: "$76" },
+    {
+      image: image37,
+      title: "Women Zip-Front Relaxed Fit Jacket",
+      price: "$85",
+    },
+    { image: image38, title: "Kid Tapered Slim Fit Trouser", price: "$90" },
+    { image: image39, title: "Men Printed Plain Cotton Shirt", price: "$120" },
+    {
+      image: image40,
+      title: "Women Zip-Front Relaxed Fit Jacket",
+      price: "$150",
+    },
+
+    { image: image41, title: "Kid Tapered Slim Fit Trouser", price: "$76" },
+    {
+      image: image42,
+      title: "Men Round Neck Pure Cotton T-shirt",
+      price: "$85",
+    },
+    {
+      image: image43,
+      title: "Boy Round Neck Pure Cotton T-shirt",
+      price: "$90",
+    },
+    {
+      image: image44,
+      title: "Women Zip-Front Relaxed Fit Jacket",
+      price: "$120",
+    },
+    {
+      image: image45,
+      title: "Men Tapered Fit Flat-Front Trousers",
+      price: "$150",
+    },
+    { image: image46, title: "PGirls Round Neck Cotton Top", price: "$76" },
+    {
+      image: image47,
+      title: "Women Zip-Front Relaxed Fit Jacket",
+      price: "$85",
+    },
+    { image: image48, title: "Kid Tapered Slim Fit Trouser", price: "$90" },
+    { image: image49, title: "Men Printed Plain Cotton Shirt", price: "$120" },
+    {
+      image: image50,
+      title: "Women Zip-Front Relaxed Fit Jacket",
+      price: "$150",
+    },
   ];
 
   return (
     <div>
       <div className="maincollection-section" id="maincollection">
         <div className="row">
-        <h1>Filters</h1>
-          <div className="filter-section">
+          <h1>Filters</h1>
+          <div className="filter-section Category">
             <h6>Categories</h6>
             <label>
               <input type="checkbox" />
@@ -73,7 +255,7 @@ const MainCollection = () => {
             </label>
           </div>
 
-          <div className="filter-section">
+          <div className="filter-section types">
             <h6>Types</h6>
             <label>
               <input type="checkbox" />
@@ -95,9 +277,9 @@ const MainCollection = () => {
             <h3>All Collections</h3>
             <form action="">
               <select name="" id="">
-                <option value="1">Sorted by: Relavent</option>
-                <option value="2">Sorted by: Low to High</option>
-                <option value="3">Sorted by: High to Low</option>
+                <option value="1">Sort by: Relavent</option>
+                <option value="2">Sort by: Low to High</option>
+                <option value="3">Sort by: High to Low</option>
               </select>
             </form>
             <div className="row">
